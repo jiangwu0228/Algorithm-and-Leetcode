b383460815c71ecdd14e08258cb12ffc980eb419
Jason Wu <jiangwu0228@gmail.com>
2022-04-02 23:20:37 +1300

    125. valid palindrome

--- project-ts/src/components/Result.tsx
+++ project-ts/src/components/Result.tsx
@@ -416,6 +416,20 @@ function Result() {
 //     }
 //   }
 // };
+//125. Valid Palindrome
+// function validPalindrome(s: string): boolean {
+//   const replacedString = s.replace(/[^a-z0-9]/ig, "").toLowerCase()
+//   let left = 0;
+//   let right = replacedString.length - 1;
+//   while (left < right) {
+//     if (replacedString[left] !== replacedString[right]) {
+//       return false;
+//     }
+//     left++;
+//     right--;
+//   }
+//   return true;
+// };
   return (
     <>
       <iframe
