50f194c04bef5ab086a66faec50fa170280abce6
Jason Wu <jiangwu0228@gmail.com>
2022-04-03 23:20:19 +1200

    744. Find Smallest Letter Greater Than Target

--- project-ts/src/components/Result.tsx
+++ project-ts/src/components/Result.tsx
@@ -473,27 +473,58 @@ function Result() {
   /**
  Do not return anything, modify nums in-place instead.
  */
-// function nextPermutation(nums: number[]): void {
-//   let i = nums.length - 2;                   // 向左遍历，i从倒数第二开始是为了nums[i+1]要存在
-//   while (i >= 0 && nums[i] >= nums[i + 1]) { // 寻找第一个小于右邻居的数
-//       i--;
-//   }
-//   if (i >= 0) {                             // 这个数在数组中存在，从它身后挑一个数，和它换
-//       let j = nums.length - 1;                // 从最后一项，向左遍历
-//       while (j >= 0 && nums[j] <= nums[i]) {  // 寻找第一个大于 nums[i] 的数
-//           j--;
-//       }
-//       [nums[i], nums[j]] = [nums[j], nums[i]]; // 两数交换，实现变大
-//   }
-//   // 如果 i = -1，说明是递减排列，如 3 2 1，没有下一排列，直接翻转为最小排列：1 2 3
-//   let l = i + 1;           
-//   let r = nums.length - 1;
-//   while (l < r) {                            // i 右边的数进行翻转，使得变大的幅度小一些
-//       [nums[l], nums[r]] = [nums[r], nums[l]];
-//       l++;
-//       r--;
-//   }
-// };
+  // function nextPermutation(nums: number[]): void {
+  //   let i = nums.length - 2;                   // 向左遍历，i从倒数第二开始是为了nums[i+1]要存在
+  //   while (i >= 0 && nums[i] >= nums[i + 1]) { // 寻找第一个小于右邻居的数
+  //       i--;
+  //   }
+  //   if (i >= 0) {                             // 这个数在数组中存在，从它身后挑一个数，和它换
+  //       let j = nums.length - 1;                // 从最后一项，向左遍历
+  //       while (j >= 0 && nums[j] <= nums[i]) {  // 寻找第一个大于 nums[i] 的数
+  //           j--;
+  //       }
+  //       [nums[i], nums[j]] = [nums[j], nums[i]]; // 两数交换，实现变大
+  //   }
+  //   // 如果 i = -1，说明是递减排列，如 3 2 1，没有下一排列，直接翻转为最小排列：1 2 3
+  //   let l = i + 1;
+  //   let r = nums.length - 1;
+  //   while (l < r) {                            // i 右边的数进行翻转，使得变大的幅度小一些
+  //       [nums[l], nums[r]] = [nums[r], nums[l]];
+  //       l++;
+  //       r--;
+  //   }
+  // };
+
+  //744. Find Smallest Letter Greater Than Target
+  // function nextGreatestLetter(letters: string[], target: string): string {
+  //   const length = letters.length;
+  //   let nextGreater = letters[0];
+  //   for (let i = 0; i < length; i++) {
+  //       if (letters[i] > target) {
+  //           nextGreater = letters[i];
+  //           break;
+  //       }
+  //   }
+  //   return nextGreater;
+  // };
+
+  // function nextGreatestLetter(letters: string[], target: string): string {
+  //   const length = letters.length;
+  //   if (target >= letters[length - 1]) {
+  //     return letters[0];
+  //   }
+  //   let low = 0,
+  //     high = length - 1;
+  //   while (low < high) {
+  //     const mid = Math.floor((high - low) / 2) + low;
+  //     if (letters[mid] > target) {
+  //       high = mid;
+  //     } else {
+  //       low = mid + 1;
+  //     }
+  //   }
+  //   return letters[low];
+  // }
 
   return (
     <>
