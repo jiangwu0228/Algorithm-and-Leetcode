6de7c59272a6445538a09910a3220596a10953e0
Jason Wu <jiangwu0228@gmail.com>
2022-04-04 23:57:34 +1200

    seek and destroy

--- project-js/src/App.js
+++ project-js/src/App.js
@@ -2,6 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+// seek and destroy
+//You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+//Note: You have to use the arguments object.
+// function destroyer(arr, ...args) {
+
+//   return arr.filter(e => !args.includes(e));
+// }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
