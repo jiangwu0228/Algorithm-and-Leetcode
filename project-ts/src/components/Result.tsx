b6654c1753ab749f8c68409354beb61c4b80b5b7
Jason Wu <jiangwu0228@gmail.com>
2022-04-09 20:29:15 +1200

    minArray binary search

--- project-ts/src/components/Result.tsx
+++ project-ts/src/components/Result.tsx
@@ -801,7 +801,22 @@ function Result() {
   //   return min;
   // };
 
-
+//   function minArray(numbers: number[]): number {
+//     //binary search
+//     let left = 0;
+//     let right = numbers.length - 1;
+//     while (left < right) {
+//       let mid = Math.floor((left + right) / 2);
+//       if (numbers[mid] > numbers[right]) {
+//         left = mid + 1;
+//       } else if(numbers[mid] < numbers[right]) {
+//         right = mid;
+//       }else {
+//         right--;
+//       }
+//     }
+//     return numbers[left];
+// };
 
   return (
     <>
