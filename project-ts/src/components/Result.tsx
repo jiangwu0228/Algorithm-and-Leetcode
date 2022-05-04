4ce36e6385660bec2e7b0eb34f73d6baf98f1e02
Jason Wu <jiangwu0228@gmail.com>
2022-05-04 20:09:53 +1200

    剑指 Offer 19. 正则表达式匹配

--- project-ts/src/components/Result.tsx
+++ project-ts/src/components/Result.tsx
@@ -2082,34 +2082,106 @@ function Result() {
   // };
 
   // 剑指 Offer 38. 字符串的排列
-  function permutation(s: string): string[] {
-    const n = s.length,
-      res = [];
-    const stateArr = Array(n).fill(false);
-    const ss = s.split("").sort().join("");
-    const dfs = (str) => {
-      if (str.length === n) {
-        res.push(str);
-        return;
+  // function permutation(s: string): string[] {
+  //   const n = s.length,
+  //     res = [];
+  //   const stateArr = Array(n).fill(false);
+  //   const ss = s.split("").sort().join("");
+  //   const dfs = (str) => {
+  //     if (str.length === n) {
+  //       res.push(str);
+  //       return;
+  //     }
+  //     for (let i = 0; i < n; i++) {
+  //       if (stateArr[i]) continue;
+  //       if (i > 0 && ss[i - 1] === ss[i] && !stateArr[i - 1]) continue;
+  //       stateArr[i] = true;
+  //       str += ss[i];
+  //       dfs(str);
+  //       stateArr[i] = false;
+  //       str = str.slice(0, str.length - 1);
+  //     }
+  //   };
+  //   dfs("");
+  //   return res;
+  // }
+
+  // 剑指 Offer 19. 正则表达式匹配
+  function isMatch(s: string, p: string): boolean {
+    //递归
+    // return isMatchIdx(s, p, 0, 0);
+    //动态规划
+    const sLen = s.length;
+    const pLen = p.length;
+    const dp = new Array<boolean[]>(sLen+1);
+    for (let idx = 0; idx < dp.length; idx++) {
+      dp[idx] = new Array<boolean>(pLen+1).fill(false);
+    }
+  
+    const isMatchIdx: (sIdx: number, pIdx: number) => boolean = (sIdx, pIdx) => {
+      return p[pIdx-2] === s[sIdx-1] || p[pIdx-2] === '.';
+    }
+  
+    // 初始化 dp
+    dp[0][0] = true;
+    for(let j = 2; j <= pLen; j += 2) {
+      //当s为空时，p必须满足a*b*.*这样的结构才能匹配成空串
+      //当s不为空，p为空为false
+      dp[0][j] = dp[0][j - 2] && p[j - 1] == '*';
+    }
+  
+    for (let i = 1; i < sLen+1; i++) {
+      for (let j = 0; j < pLen+1; j++) {
+        if (p[j-1] === '*') {
+          if (isMatchIdx(i, j)) {
+            dp[i][j] = dp[i][j-2] || dp[i-1][j];
+          } else {
+            dp[i][j] = dp[i][j-2];
+          }
+        } else {
+          if (isMatchIdx(i, j+1)) {
+            dp[i][j] = dp[i-1][j-1];
+          } else {
+            dp[i][j] =false;
+          }
+        }
       }
-      for (let i = 0; i < n; i++) {
-        if (stateArr[i]) continue;
-        if (i > 0 && ss[i - 1] === ss[i] && !stateArr[i - 1]) continue;
-        stateArr[i] = true;
-        str += ss[i];
-        dfs(str);
-        stateArr[i] = false;
-        str = str.slice(0, str.length - 1);
-      }
-    };
-    dfs("");
-    return res;
+    }
+  
+    return dp[sLen][pLen];
   }
 
-  /**
-   * Your functions will be called as such:
-   * deserialize(serialize(root));
-   */
+  // function isMatchIdx(
+  //   s: string,
+  //   p: string,
+  //   sIdx: number,
+  //   pIdx: number
+  // ): boolean {
+  //   if (!s[sIdx] && !p[pIdx]) {
+  //     return true;
+  //   }
+  //   if (!p[pIdx] && s[sIdx]) {
+  //     return false;
+  //   }
+  //   if (p[pIdx + 1] === "*") {
+  //     if (s[sIdx] === p[pIdx] || (p[pIdx] === "." && s[sIdx])) {
+  //       // s 不能为空的原因： '.'正则要求必须至少有一个字符
+  //       return (
+  //         isMatchIdx(s, p, sIdx + 1, pIdx + 2) || // 例子：s === 'dbc', p === 'a*bc'
+  //         isMatchIdx(s, p, sIdx + 1, pIdx) || // 例子：s === 'aaabc', p === 'a*bc'
+  //         isMatchIdx(s, p, sIdx, pIdx + 2)
+  //       ); // 例子： s === 'abc', p === 'a*abc'
+  //     } else {
+  //       return isMatchIdx(s, p, sIdx, pIdx + 2); // 例子： s === 'bc', p === 'a*bc'
+  //     }
+  //   }
+
+  //   if (s[sIdx] === p[pIdx] || (p[pIdx] === "." && s[sIdx])) {
+  //     return isMatchIdx(s, p, sIdx + 1, pIdx + 1); // 若第一个字符匹配上，则模式和字符串比较位置向后移一位
+  //   }
+
+  //   return false;
+  // }
 
   return (
     <>
