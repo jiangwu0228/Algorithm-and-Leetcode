7505192fed9361437f23b92ff3b73aa64568e45e
Jason Wu <jiangwu0228@gmail.com>
2022-05-07 21:35:03 +1200

    1757 可回收且低脂的产品

--- project-ts/src/components/10days.sql
+++ project-ts/src/components/10days.sql
@@ -1,6 +1,10 @@
 # 595. 大的国家
 
-SELECT name, population, area FROM world WHERE population >= 25000000 OR area >= 3000000;
+SELECT name, population, area FROM World WHERE population >= 25000000 OR area >= 3000000;
 
-SELECT name, population, area FROM world WHERE population >= 25000000 UNION SELECT name, population, area FROM world WHERE area >= 3000000;
+SELECT name, population, area FROM World WHERE population >= 25000000 UNION SELECT name, population, area FROM world WHERE area >= 3000000;
+
+#1757. 可回收且低脂的产品
+
+SELECT product_id FROM Products WHERE recyclable = 'Y' and low_fats = 'Y';
 
