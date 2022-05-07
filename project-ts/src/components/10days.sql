9b63360bf0ebc05ea2a5a60c85b00d96fe37c595
Jason Wu <jiangwu0228@gmail.com>
2022-05-07 21:39:28 +1200

    183. 从不订购的客户

--- project-ts/src/components/10days.sql
+++ project-ts/src/components/10days.sql
@@ -4,10 +4,19 @@ SELECT name, population, area FROM World WHERE population >= 25000000 OR area >=
 
 SELECT name, population, area FROM World WHERE population >= 25000000 UNION SELECT name, population, area FROM world WHERE area >= 3000000;
 
-#1757. 可回收且低脂的产品
+# 1757. 可回收且低脂的产品
 
 SELECT product_id FROM Products WHERE recyclable = 'Y' and low_fats = 'Y';
 
 # 584. 寻找用户推荐人
 
 SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS NULL;
+
+# 183. 从不订购的客户
+
+SELECT Customers.name as 'Customers'
+FROM Customers
+WHERE Customers.id NOT IN 
+(
+    SELECT customerId FROM Orders
+);
