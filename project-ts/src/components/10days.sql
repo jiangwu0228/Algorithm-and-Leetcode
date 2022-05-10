b0f58b72cec382f598ddbe1ad0fd79cf6f81fdf8
Jason Wu <jiangwu0228@gmail.com>
2022-05-10 16:10:46 +1200

    1795. 每个产品在不同商店的价格

--- project-ts/src/components/10days.sql
+++ project-ts/src/components/10days.sql
@@ -79,3 +79,17 @@ SELECT employee_id FROM
 GROUP BY employee_id
 HAVING COUNT(employee_id) = 1
 ORDER BY employee_id;
+
+--1795. 每个产品在不同商店的价格
+
+select product_id, 'store1' as store, store1 as price 
+from products
+where store1 is not null
+union all
+select product_id, 'store2' as store, store2 as price
+from products
+where store2 is not null
+union all
+select product_id, 'store3' as store, store3 as price
+from products
+where store3 is not null
