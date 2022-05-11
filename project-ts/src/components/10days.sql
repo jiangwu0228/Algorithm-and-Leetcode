b6c2d8ad60e79af621cf9d12d827b8fa4d395297
Jason Wu <jiangwu0228@gmail.com>
2022-05-11 23:20:00 +1200

    1148. 文章浏览 I

--- project-ts/src/components/10days.sql
+++ project-ts/src/components/10days.sql
@@ -160,3 +160,10 @@ where visit_id not in (
     group by visit_id
 )
 group by customer_id;
+
+-- 1148. 文章浏览 I
+
+select distinct author_id as id
+from Views
+where author_id = viewer_id
+order by author_id;
