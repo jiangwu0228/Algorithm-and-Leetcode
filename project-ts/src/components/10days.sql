e91668172833372738fc082ac93dd86d2e333b35
Jason Wu <jiangwu0228@gmail.com>
2022-05-13 23:06:04 +1200

    1141. 查询近30天活跃用户数

--- project-ts/src/components/10days.sql
+++ project-ts/src/components/10days.sql
@@ -192,3 +192,10 @@ LEFT JOIN
 WHERE
     c.name = 'RED'
 );
+
+-- 1141. 查询近30天活跃用户数
+
+select activity_date day, count(distinct user_id) active_users
+from activity
+where datediff('2019-07-27', activity_date) >= 0 AND datediff('2019-07-27', activity_date) <30
+group by activity_date
