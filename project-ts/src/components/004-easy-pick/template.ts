b06e8cd1e87cb17c511c0cf98f1569ed7b2a2b30
Jason Wu <jiangwu0228@gmail.com>
2022-05-18 23:51:28 +1200

    004-add js method

--- project-ts/src/components/004-easy-pick/template.ts
+++ project-ts/src/components/004-easy-pick/template.ts
@@ -2,6 +2,20 @@ type MyPick<T, K extends keyof T> = {
     [P in K]: T[P];
 }
 
+// if in js will be
+
+// function myPick (obj, keys) {
+//     const result = {};
+
+//     for (const key in obj) {
+//         if (keys.includes(key)) {
+//             result[key] = obj[key];
+//         }
+//     }
+
+//     return result;
+// }
+
 // return a obj
 // foreach key in obj: [P in K]
 // get value: T[P]
